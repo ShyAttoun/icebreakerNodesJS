cb5514e6faa504b78396fde6fbd2537ff08f19ee
shay <shupit21@gmail.com>
2019-06-20 12:23:19 +0300

    sqljune

--- index.js
+++ index.js
@@ -54,22 +54,21 @@ app.get('/',function(req,res){
 
 });
 
-app.get('/api/jokes',(req,res) => {
-
+app.get('/db', function (req, res) {
   var SQL = "CREATE TABLE Jokes(id SERIAL, setup TEXT, punchline TEXT)"
-    
+  
   pool.query(SQL,function(err,dbResult){
   
     if(err){
       res.json(err);
     }else{
-      res.json(dbResult);  
-  }
-  res.send(jokes);
-});
+      res.json(dbResult);
+    }
+  });
 
-app.get('/api/interests',(req,res) => {
-  res.send(interests);
+app.get('/api/jokes',(req,res) => {
+
+  res.send(jokes);
 });
 
 app.get('/api/jokes/:id',(req,res) => {
