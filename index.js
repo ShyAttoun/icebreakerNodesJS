45293afbdb29df841fd1f7e23e0a2357055cdb83
shay <shupit21@gmail.com>
2019-06-29 19:18:42 +0300

    sqljune

--- index.js
+++ index.js
@@ -69,13 +69,13 @@ app.post('/api/jokes',(req,res)=>{
   const {error} = validateCourse(req.body); // result.error
   if (error) return res.status(400).send(error.details[0].message);
 
-  const joker = {
-    id: jokes.length + 1,
-    setup: req.body.setup,
-   punchline: req.body.punchline
-  };
-  jokes.push(joker);
-  res.send(joker);
+  // const joker = {
+  //   id: jokes.length + 1,
+  //   setup: req.body.setup,
+  //  punchline: req.body.punchline
+  // };
+  // jokes.push(joker);
+  // res.send(joker);
 
   var setup = req.query.setup
   var punchline = req.query.punchline
