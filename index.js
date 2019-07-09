9ac621a8a1eb7ec31a6b287f0f71a41a03e0db0a
shay <shupit21@gmail.com>
2019-07-09 20:30:46 +0300

    sqljune

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ const movies = [];
 app.get('/',function(req,res){
     res.send('welcome to IceBreaker App brrrro!')});
 
-    const pool = new Pool ({
-      connectionString: process.env.DATABASE_URL,ssl: true
-      });
+    // const pool = new Pool ({
+    //   connectionString: process.env.DATABASE_URL,ssl: true
+    //   });
       
   // app.get('/db', function (req, res) {
   //     var SQL = "CREATE TABLE Jokes(id SERIAL, setup TEXT, punchline TEXT)"
