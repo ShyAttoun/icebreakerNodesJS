36807ebbc246c7b7cde9d262e48f4ee165ad94c4
shay <shupit21@gmail.com>
2019-07-10 12:20:24 +0300

    sqljune

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(morgan('combined'))
 // // parse application/json
 // app.use(bodyParser.json())
 
- app.use(express.json());
+// app.use(express.json());
 
 // const jokes = [];
 // const pickuplines = [];
@@ -28,25 +28,11 @@ app.use(morgan('combined'))
 
 
 app.get('/',function(req,res){
-    res.send('welcome to IceBreaker App brrrro!')
-  });
+    res.send('welcome to IceBreaker App brrrro!')});
 
     // const pool = new Pool ({
     //   connectionString: process.env.DATABASE_URL,ssl: true
     //   });
-      
-  // app.get('/db', function (req, res) {
-  //     var SQL = "CREATE TABLE Jokes(id SERIAL, setup TEXT, punchline TEXT)"
-      
-  //     pool.query(SQL,function(err,dbResult){
-      
-  //       if(err){
-  //         res.json(err);
-  //       }else{
-  //         res.json(dbResult);
-  //       }
-  //     });
-  //     });
 
 app.get('/api/jokes',(req,res) => {
   const connection = mysql.createConnection({
@@ -72,168 +58,158 @@ app.get('/api/jokes',(req,res) => {
     })
     res.json(jokes)
   })
-// app.get('/api/jokes/:id',(req,res) => {
-//   const joke = jokes.find(c => c.id === parseInt(req.params.id));
-//   if (!joke) return res.status(404).send('the course coudlnt be found');
-//   res.send(joke)
-// });
+  
+app.get('/api/jokes/:id',(req,res) => {
+  const joke = jokes.find(c => c.id === parseInt(req.params.id));
+  if (!joke) return res.status(404).send('the course coudlnt be found');
+  res.send(joke)
+});
 
-// app.delete('/api/jokes/:id',(req,res) => {
-//   //checks is the adress exists
-//   const joke = jokes.find(c => c.id === parseInt(req.params.id));
-//   if (!joke) return res.status(404).send('the course coudlnt be found');
+app.delete('/api/jokes/:id',(req,res) => {
+  //checks is the adress exists
+  const joke = jokes.find(c => c.id === parseInt(req.params.id));
+  if (!joke) return res.status(404).send('the course coudlnt be found');
 
-//   //delete part
-//   const index = jokes.indexOf(joke);
-//   jokes.splice(index,1);
+  //delete part
+  const index = jokes.indexOf(joke);
+  jokes.splice(index,1);
 
-//   //returnibg the same course
-//   res.send(joke);
+  //returnibg the same course
+  res.send(joke);
 
-// });
+});
 
-// app.post('/api/jokes',(req,res)=>{
+app.post('/api/jokes',(req,res)=>{
 
-//   const {error} = validateCourse(req.body); // result.error
-//   if (error) return res.status(400).send(error.details[0].message);
+  const {error} = validateCourse(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
 
-  // const joker = {
-  //   id: jokes.length + 1,
-  //   setup: req.body.setup,
-  //  punchline: req.body.punchline
-  // };
-  // jokes.push(joker);
-  // res.send(joker);
+  const joker = {
+    id: jokes.length + 1,
+    setup: req.body.setup,
+   punchline: req.body.punchline
+  };
+  jokes.push(joker);
+  res.send(joker);
 
-  // var SQL = "INSERT INTO Jokes(setup,punchline) VALUES($1,$2);"
-  // var values = [setup,punchline]
+  });
 
-  // pool.query(SQL,values,function(err,dbResult){
-          
-  //   if(err){
-  //     res.json(err);
-  //   }else{
-  //     res.json(dbResult);
-  //   }
-  // });
-  // });
-
-  // app.put('/api/jokes/:id',(req,res) => {
-  //   const joke = jokes.find(c => c.id === parseInt(req.params.id));
-  //    if (!joke) return res.status(404).send('the course coudlnt be found');
+  app.put('/api/jokes/:id',(req,res) => {
+    const joke = jokes.find(c => c.id === parseInt(req.params.id));
+     if (!joke) return res.status(404).send('the course coudlnt be found');
     
-  //    const {error} = validateCourse(req.body); // result.error
-  //    if (error) return res.status(400).send(error.details[0].message);
+     const {error} = validateCourse(req.body); // result.error
+     if (error) return res.status(400).send(error.details[0].message);
        
-  //    joke.name = req.body.name;
-  //    res.send(joke);
-  //  });
+     joke.name = req.body.name;
+     res.send(joke);
+   });
 //--------------------------------------------------------------//
 //same method different
-//    app.get('/api/pickuplines',(req,res) => {
-//     res.send(pickuplines);
-//   });
+   app.get('/api/pickuplines',(req,res) => {
+    res.send(pickuplines);
+  });
   
-//   app.get('/api/pickuplines/:id',(req,res) => {
-//     const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
-//     if (!pickup) return res.status(404).send('the course coudlnt be found');
-//     res.send(pickup)
-//   });
+  app.get('/api/pickuplines/:id',(req,res) => {
+    const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
+    if (!pickup) return res.status(404).send('the course coudlnt be found');
+    res.send(pickup)
+  });
   
-//   app.delete('/api/pickuplines/:id',(req,res) => {
-//     //checks is the adress exists
-//     const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
-//     if (!pickup) return res.status(404).send('the course coudlnt be found');
+  app.delete('/api/pickuplines/:id',(req,res) => {
+    //checks is the adress exists
+    const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
+    if (!pickup) return res.status(404).send('the course coudlnt be found');
   
-//     //delete part
-//     const index = pickuplines.indexOf(pickup);
-//     pickuplines.splice(index,1);
+    //delete part
+    const index = pickuplines.indexOf(pickup);
+    pickuplines.splice(index,1);
   
-//     //returnibg the same course
-//     res.send(pickup);
+    //returnibg the same course
+    res.send(pickup);
   
-//   });
-  
-  
+  });
   
   
-//   app.post('/api/pickuplines',(req,res)=>{
-//     const {error} = validatePickUpLine(req.body); // result.error
-//     if (error) return res.status(400).send(error.details[0].message);
   
-//     const pickupliner = {
-//       id: pickuplines.length + 1,
-//       setup: req.body.setup
-//     };
   
-//     pickuplines.push(pickupliner);
-//     res.send(pickupliner);
-//     });
+  app.post('/api/pickuplines',(req,res)=>{
+    const {error} = validatePickUpLine(req.body); // result.error
+    if (error) return res.status(400).send(error.details[0].message);
   
-//     app.put('/api/pickuplines/:id',(req,res) => {
+    const pickupliner = {
+      id: pickuplines.length + 1,
+      setup: req.body.setup
+    };
   
-//       const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
-//        if (!pickup) return res.status(404).send('the course coudlnt be found');
+    pickuplines.push(pickupliner);
+    res.send(pickupliner);
+    });
+  
+    app.put('/api/pickuplines/:id',(req,res) => {
+  
+      const pickup = pickuplines.find(c => c.id === parseInt(req.params.id));
+       if (!pickup) return res.status(404).send('the course coudlnt be found');
       
-//        const {error} = validatePickUpLine(req.body); // result.error
-//        if (error) return res.status(400).send(error.details[0].message);
+       const {error} = validatePickUpLine(req.body); // result.error
+       if (error) return res.status(400).send(error.details[0].message);
          
-//        pickup.name = req.body.name;
-//        res.send(pickup);
+       pickup.name = req.body.name;
+       res.send(pickup);
      
-//      });
+     });
   
-// //------------------------------------------------------------------------//
+//------------------------------------------------------------------------//
 
-// app.get('/api/funnyfacts',(req,res) => {
-//   res.send(funnyfacts);
-// });
+app.get('/api/funnyfacts',(req,res) => {
+  res.send(funnyfacts);
+});
 
-// app.get('/api/funnyfacts/:id',(req,res) => {
-//   const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
-//   if (!funnyfact) return res.status(404).send('the course coudlnt be found');
-//   res.send(funnyfact)
-// });
+app.get('/api/funnyfacts/:id',(req,res) => {
+  const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
+  if (!funnyfact) return res.status(404).send('the course coudlnt be found');
+  res.send(funnyfact)
+});
 
-// app.delete('/api/funnyfacts/:id',(req,res) => {
-//   //checks is the adress exists
-//   const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
-//   if (!funnyfact) return res.status(404).send('the course coudlnt be found');
+app.delete('/api/funnyfacts/:id',(req,res) => {
+  //checks is the adress exists
+  const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
+  if (!funnyfact) return res.status(404).send('the course coudlnt be found');
 
-//   //delete part
-//   const index = funnyfacts.indexOf(funnyfact);
-//   funnyfacts.splice(index,1);
+  //delete part
+  const index = funnyfacts.indexOf(funnyfact);
+  funnyfacts.splice(index,1);
 
-//   //returnibg the same course
-//   res.send(funnyfact);
+  //returnibg the same course
+  res.send(funnyfact);
 
-// });
+});
 
-// app.post('/api/funnyfacts',(req,res)=>{
-//   const {error} = validateFunnyFacts(req.body); // result.error
-//   if (error) return res.status(400).send(error.details[0].message);
+app.post('/api/funnyfacts',(req,res)=>{
+  const {error} = validateFunnyFacts(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
 
-//   const funnyfactor = {
-//     id: funnyfacts.length + 1,
-//     setup: req.body.setup
-//   };
+  const funnyfactor = {
+    id: funnyfacts.length + 1,
+    setup: req.body.setup
+  };
 
-//   funnyfacts.push(funnyfactor);
-//   res.send(funnyfactor);
-//   });
+  funnyfacts.push(funnyfactor);
+  res.send(funnyfactor);
+  });
 
-//   app.put('/api/funnyfacts/:id',(req,res) => {
+  app.put('/api/funnyfacts/:id',(req,res) => {
 
-//     const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
-//      if (!funnyfact) return res.status(404).send('the course coudlnt be found');
+    const funnyfact = funnyfacts.find(c => c.id === parseInt(req.params.id));
+     if (!funnyfact) return res.status(404).send('the course coudlnt be found');
     
-//      const {error} = validateFunnyFacts(req.body); // result.error
-//      if (error) return res.status(400).send(error.details[0].message);
+     const {error} = validateFunnyFacts(req.body); // result.error
+     if (error) return res.status(400).send(error.details[0].message);
        
-//      funnyfact.name = req.body.name;
-//      res.send(funnyfact);
+     funnyfact.name = req.body.name;
+     res.send(funnyfact);
    
-//    });
+   });
 
 
 
